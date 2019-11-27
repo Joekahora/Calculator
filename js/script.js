f03113a8afcda4b0efc4cdc5abe02a5f140feb5b
JoeKahora <gachau.joel@gmail.com>
2019-11-27 14:19:07 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var add = function(firstNumber, secondNumber){
+	return firstNumber + secondNumber;
+};
+var number1 = parseInt(prompt("Enter the first Number: "));
+var number2 = parseInt(prompt("Enter the second Number: "));
+alert(add(number1, number2));
