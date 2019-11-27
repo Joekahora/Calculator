1fe7c50cd9aeda486e45ff0175a3a03506d1437c
JoeKahora <gachau.joel@gmail.com>
2019-11-27 14:30:51 +0300

    tried out different operators on it.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var add = function(firstNumber, secondNumber){
-	return firstNumber + secondNumber;
+var modulos = function(firstNumber, secondNumber){
+	return firstNumber % secondNumber;
 };
 var number1 = parseInt(prompt("Enter the first Number: "));
 var number2 = parseInt(prompt("Enter the second Number: "));
-alert(add(number1, number2));
+alert(modulos(number1, number2));
